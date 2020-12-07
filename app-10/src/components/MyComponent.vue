9937ede64b3c0364f623360473af458ad5911598
jod35 <jodestrevon@gmail.com>
2020-12-07 14:10:23 -0500

    created a single file component

---
+++ app-10/src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+          <h2>{{getGreeting}}</h2>
+   <p>This is my HELLO WORLD COMPONENT</p>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'MyComponent',
+    data(){
+        return{
+            reversedGreeting:" !dlroW olleH"
+        }
+    }
+}
+</script>
+
+
+<style lang="css" scoped>
+h2{
+    text-align: center;
+}    
+</style>
--- app-9/main.js
+++ app-9/main.js
@@ -1,52 +1,56 @@
-const posts=[
-    {
-        id:1,
-        name:"Jail in Uganda",
-        author:"jeje",
-        likes:1,
-        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!"
-    },
-    {
-        id:2,
-        name:"The rise of VueJS",
-        author:"jannet",
-        likes:0,
-        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!"
-    },
-    {
-        id:3,
-        name:"Vue is awesome",
-        author:"jordan",
-        likes:12,
-        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!"
-    },
-    {
-        id:4,
-        name:"I am Stressed out",
-        author:"jerry",
-        likes:3,
-        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!"
-    },
-    {
-        id:5,
-        name:"Politics in Uganda",
-        author:"jona",
-        likes:7,
-        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!"
-    },
-    {
-        id:6,
-        name:"Jail in Uganda",
-        author:"jonathan",
-        likes:13,
-        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!"
-    },
-]
+const posts = [
+  {
+    id: 1,
+    name: "Jail in Uganda",
+    author: "jeje",
+    likes: 1,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!",
+  },
+  {
+    id: 2,
+    name: "The rise of VueJS",
+    author: "jannet",
+    likes: 0,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!",
+  },
+  {
+    id: 3,
+    name: "Vue is awesome",
+    author: "jordan",
+    likes: 12,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!",
+  },
+  {
+    id: 4,
+    name: "I am Stressed out",
+    author: "jerry",
+    likes: 3,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!",
+  },
+  {
+    id: 5,
+    name: "Politics in Uganda",
+    author: "jona",
+    likes: 7,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!",
+  },
+  {
+    id: 6,
+    name: "Jail in Uganda",
+    author: "jonathan",
+    likes: 13,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam harum laudantium adipisci corporis tenetur possimus voluptates quia repellendus aperiam!",
+  },
+];
 
-
-
-const postComponent={
-            template:`
+const postComponent = {
+  template: `
                 <div>
                     <h3 class="head">{{post.name}}</h3>
                     <p class="head">by {{post.author}}</p>
@@ -62,26 +66,27 @@ const postComponent={
                 </div>
             
             `,
-            props:['post','posts'],
-            methods:{
-                addLike(postID){
-                    let post =this.posts.find(post => post.id == postID);
+  props: ["post", "posts"],
+  methods: {
+    addLike(postID) {
+      let post = this.posts.find((post) => post.id == postID);
 
-                    post.likes++;
-                }
-            }
-}
+      post.likes++;
+    },
+  },
+};
 new Vue({
-    el:"#app",
-    data:{ posts:posts},
-    computed:{
-        sortedPosts(){
-            return this.posts.sort((a,b)=>{
-                return b.likes - a.likes
-            })
-        }},
+  el: "#app",
+  data: { posts: posts },
+  computed: {
+    sortedPosts() {
+      return this.posts.sort((a, b) => {
+        return b.likes - a.likes;
+      });
+    },
+  },
 
-    components:{
-        'post-component':postComponent
-    }
-})
+  components: {
+    "post-component": postComponent,
+  },
+});
